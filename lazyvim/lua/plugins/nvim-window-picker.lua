8a7e99fa3a210cbbd87fb005444be7e0af37a68b
Pedro Castillo <pacp95@gmail.com>
2025-01-03 00:16:21 +0000

    Add window picker action

--- lazyvim/lua/plugins/nvim-window-picker.lua
+++ lazyvim/lua/plugins/nvim-window-picker.lua
@@ -1,10 +1,22 @@
 return {
   {
-    's1n7ax/nvim-window-picker',
-    name = 'window-picker',
-    event = 'VeryLazy',
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
     opts = {
-      hint = 'floating-big-letter',
+      hint = "floating-big-letter",
+    },
+    keys = {
+      {
+        "<Leader>ww",
+        function()
+          local picked_window_id = require("window-picker").pick_window()
+          if picked_window_id then
+            vim.api.nvim_set_current_win(picked_window_id)
+          end
+        end,
+        desc = "Pick Window",
+      },
     },
   },
 }
