a62094adf5c2ccd5ed62d3163cd1f58e2a612bf6
Pedro Castillo <pacp95@gmail.com>
2026-01-20 16:45:48 -0600

    remove catppucchin workaround for bufferline

--- lazyvim/lua/plugins/catppucchin.lua
+++ lazyvim/lua/plugins/catppucchin.lua
@@ -52,13 +52,4 @@ return {
       colorscheme = "catppuccin",
     },
   },
-  {
-    "akinsho/bufferline.nvim",
-    init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
-  },
 }
