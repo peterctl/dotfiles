bb56dbb5c1328a21d931ca232aeeee2dc8a5342c
Pedro Castillo <pacp95@gmail.com>
2026-02-23 09:55:08 -0600

    Add Trouble buffer diagnostics keybinding

---
+++ lazyvim/lua/plugins/trouble.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/trouble.nvim",
+    keys = {
+      {
+        "<Leader>cD",
+        "<cmd>Trouble diagnostics toggle pinned=true focus=true<cr>",
+        desc = "Buffer diagnostics",
+      },
+    },
+  },
+}
