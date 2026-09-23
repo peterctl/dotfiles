f6b38756820f1c24d428585c8e9c5aafea769109
Pedro Castillo <pacp95@gmail.com>
2026-09-22 22:35:16 -0600

    Remove cat and less aliases for colorize.

--- zsh/colorize.zsh
+++ zsh/colorize.zsh
@@ -14,6 +14,3 @@ CHROMA_URL="https://github.com/alecthomas/chroma/releases/download/v${CHROMA_VER
 if ! which chroma >/dev/null; then
   curl -fL $CHROMA_URL | tar -xz -C $HOME/.local/bin/ chroma
 fi
-
-alias cat=ccat
-alias less=cless
